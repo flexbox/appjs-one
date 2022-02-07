eceb1e3b1435dd16dbf5e0e61cca9729226eee78
jonsamp <sampjon@gmail.com>
2022-02-07 02:26:08 -0500

    updates default metro config

--- metro.config.js
+++ metro.config.js
@@ -3,7 +3,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 
 module.exports = {
+  ...defaultConfig,
   resolver: {
+    ...defaultConfig.resolver,
     assetExts: [...defaultConfig.resolver.assetExts, 'db'],
   },
 };
